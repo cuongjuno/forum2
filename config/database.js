af4b911b99b9f413d98801d05ab162c97cd83b20
juno <cuongjunocn1@gmail.com>
2021-06-29 17:52:57 +0700

    dadsa

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-module.exports = new Sequelize('forum', 'root', process.env.DB_PW, {
-  host: 'localhost',
+module.exports = new Sequelize('heroku_6e3d4d1918875d3', 'b877a4978b95ed', '8db4b3e2', {
+  host: 'us-cdbr-east-04.cleardb.com',
   dialect: 'mysql'
 });
 
